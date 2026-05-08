892a96d5ed1cd03dded8b98725259a2895bb29a4
Mathias Flores Hoyos <aki.chainsaw24@gmail.com>
2026-05-08 13:58:25 -0500

    Implement basic calculator with user menu

---
+++ main.py
@@ -0,0 +1,49 @@
+from calculadora import sumar, restar, multiplicar, dividir
+
+def pedir_numeros():
+    a = float(input("Ingrese el primer número: "))
+    b = float(input("Ingrese el segundo número: "))
+    return a, b
+
+def menu():
+    print("\n1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("0. Salir")
+
+def main():
+    while True:
+        menu()
+        opcion = input("Seleccione una opción: ")
+
+        match opcion:
+            case "1":
+                a, b = pedir_numeros()
+                print("Resultado:", sumar(a, b))
+
+            case "2":
+                a, b = pedir_numeros()
+                print("Resultado:", restar(a, b))
+
+            case "3":
+                a, b = pedir_numeros()
+                print("Resultado:", multiplicar(a, b))
+
+            case "4":
+                a, b = pedir_numeros()
+
+                if b == 0:
+                    print("No se puede dividir entre 0")
+                else:
+                    print("Resultado:", dividir(a, b))
+
+            case "0":
+                print("Programa finalizado")
+                break
+
+            case _:
+                print("Opción inválida")
+
+if __name__ == "__main__":
+    main()
